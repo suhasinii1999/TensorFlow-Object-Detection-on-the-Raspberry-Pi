00046864a2a556e94eabcde224e8907ffec5de83
suhasinii1999 <81688529+suhasinii1999@users.noreply.github.com>
2021-06-21 13:05:30 +0530

    Update Object_detection_picamera.py

--- Object_detection_picamera.py
+++ Object_detection_picamera.py
@@ -80,7 +80,7 @@ category_index = label_map_util.create_category_index(categories)
 detection_graph = tf.Graph()
 with detection_graph.as_default():
     od_graph_def = tf.GraphDef()
-    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+    with tf.io.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
         serialized_graph = fid.read()
         od_graph_def.ParseFromString(serialized_graph)
         tf.import_graph_def(od_graph_def, name='')
